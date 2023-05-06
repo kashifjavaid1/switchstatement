a2643c30438f01439b7a5a5af56a85331cabc99e
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-06 22:45:06 +0500

    user inter marks in prompt  find grade

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// var grade = +prompt("enter marks");
+var grade = +prompt("enter marks");
 
-// switch (true) {
-//     case grade >= 90 && grade <= 100:
-//         console.log("Grade A");
-//         break;
-//     case grade >= 80 && grade < 90:
-//         console.log("Grade B");
-//         break;
-//     case grade >= 70 && grade < 80:
-//         console.log("Grade C");
-//         break;
-//     case grade >= 60 && grade < 70:
-//         console.log("Grade D");
-//         break;
-//     case grade >= 50 && grade < 60:
-//         console.log("Grade E");
-//         break;
-//     default:
-//         console.log("Grade F");
-//         break;
-// }
+switch (true) {
+    case grade >= 90 && grade <= 100:
+        console.log("Grade A");
+        break;
+    case grade >= 80 && grade < 90:
+        console.log("Grade B");
+        break;
+    case grade >= 70 && grade < 80:
+        console.log("Grade C");
+        break;
+    case grade >= 60 && grade < 70:
+        console.log("Grade D");
+        break;
+    case grade >= 50 && grade < 60:
+        console.log("Grade E");
+        break;
+    default:
+        console.log("Grade F");
+        break;
+}
 var grade="C";
 switch (grade) {
     case "A":

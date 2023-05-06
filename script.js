462e6ebee74a2438a7a79e817bf9b7fd52e156ce
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-06 22:44:08 +0500

    grade find

---
+++ script.js
@@ -0,0 +1,36 @@
+// var grade = +prompt("enter marks");
+
+// switch (true) {
+//     case grade >= 90 && grade <= 100:
+//         console.log("Grade A");
+//         break;
+//     case grade >= 80 && grade < 90:
+//         console.log("Grade B");
+//         break;
+//     case grade >= 70 && grade < 80:
+//         console.log("Grade C");
+//         break;
+//     case grade >= 60 && grade < 70:
+//         console.log("Grade D");
+//         break;
+//     case grade >= 50 && grade < 60:
+//         console.log("Grade E");
+//         break;
+//     default:
+//         console.log("Grade F");
+//         break;
+// }
+var grade="C";
+switch (grade) {
+    case "A":
+        console.log("Grade A");
+        break;
+        case "B":
+            console.log("Grade B");
+            break;
+
+    default:
+        console.log("Grade F");
+
+        break;
+}
